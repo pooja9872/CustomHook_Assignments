65b491ebd785f63a21ec843b7ee126aeecb20182
pooja9872 <pooja_fw10_055@masai.school>
2021-12-19 00:09:17 +0530

    todo

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import { Container } from '@mui/material';
-import { useTimeout } from './Hooks/CustomHooks';
 
-import GithubSearch from './Components/GithubSearch';
+import Home from './Routes/Home';
 
 function App() {
-	const isReady = useTimeout(5000);
-
 	return (
 		<div className='App'>
 			<Container>
-				<h1>Hello, Hola!</h1>
-				<h2>{isReady ? 'Sorry was stuck in traffic! 😅' : "he's on his way 🏃 🏃 🏃"}</h2>
-				<GithubSearch />
+				<Home />
 			</Container>
 		</div>
 	);
